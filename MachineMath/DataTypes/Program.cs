cbfec673e844e04066dc114da12be8e5de4b83dd
ffff1111ff <ekot1112@gmail.com>
2017-02-20 19:57:36 +0200

    tasks from second lesson

---
+++ MachineMath/DataTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //task 2
+            double pi = 3.141592653f;
+            double eyler = 2.7182818284590452;
+
+
+            Console.WriteLine(pi + ", " + eyler);
+
+            //task 3
+            string s1 = "\nMy string №1";
+            string s2 = "\tMy string №2";
+            string s3 = "\aMy string №3";
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.ReadLine();
+
+        }
+    }
+}
